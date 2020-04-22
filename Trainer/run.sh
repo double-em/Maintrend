837ec5909efbe26a5e281b824184bc1303883788
Zxited <mmgamesfull@gmail.com>
2020-04-23 00:45:39 +0200

    Path change

--- Trainer/run.sh
+++ Trainer/run.sh
@@ -1,5 +1,4 @@
-# sudo rm -rf /home/marianne/container-data/LSTMdata/*
-path=/home/$(whoami)/container-data/LSTMdata/$(date '+%F@%H;%M;%S')
+path=/home/$(whoami)/container-data/logs/$(date '+%F@%H;%M;%S')
 mkdir -p $path
 docker build -t trainer:latest .
 docker run -it --rm --gpus all -v $path:/logs trainer:latest
