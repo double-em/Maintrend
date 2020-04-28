163f0fbc83bb171335f86d5a8f36e7143c98b6ec
Zxited <mmgamesfull@gmail.com>
2020-04-28 02:56:51 +0200

    API Tester script

---
+++ Trainer/api_test.py
@@ -0,0 +1,56 @@
+import numpy as np
+import requests
+import json
+import importlib
+api = importlib.import_module("API_Puller")
+
+from sklearn.preprocessing import MinMaxScaler
+
+
+
+_back_in_time = 60
+_step = 1
+_target_size = 1
+
+train = api.pulldata2()
+
+def handle_data(dataset, target, start_index, end_index, history_size, target_size, step, single_step=False):
+    
+    data = []
+    labels = []
+
+    for i in range(0, len(dataset) - history_size, step):
+        seq = dataset[i:i + history_size]
+        label = target[i + history_size - 1]
+        data.append(seq)
+        labels.append(label)
+
+    return data, labels
+
+scaler = MinMaxScaler(feature_range=(0,1))
+rescaledX = scaler.fit_transform(train[:,2:-1])
+
+rescaledX = np.hstack((rescaledX, train[:,1:2]))
+
+print("Making timestep sets (Step size: %s, History: %s days, Target value size: %s day(s))" % (_step, _back_in_time, _target_size))
+
+X, y = handle_data(
+    rescaledX, train[:, -1], 
+    0, 
+    len(train), 
+    _back_in_time, 
+    _target_size,
+    _step, 
+    single_step=True)
+
+json_str = ""
+for m in X:
+    for n in m:
+        pass
+data = json.dumps({"signature_name":"serving_default", "instances":X[0][0]})
+
+headers = {"content-type":"application/json"}
+json_response = requests.post("http://localhost:8501/v1/models/prod:predict", data=data, headers=headers)
+predictions = json.loads(json_response.text)['predictions']
+
+print("First prediction:", predictions[0])
