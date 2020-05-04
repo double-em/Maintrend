75780b42964113af965c55a712cbe3a02e69b323
Zxited <mmgamesfull@gmail.com>
2020-05-04 18:18:41 +0200

    only build when no image

--- trainer/run.sh
+++ trainer/run.sh
@@ -5,6 +5,10 @@ path_code=$(pwd)/trainer/
 mkdir -p $path_logs
 mkdir -p $path_models
 
+if [ "$(docker images -q maintrend-trainer:latest)" == "" ]; then
+    docker build -t maintrend-trainer:latest -f trainer/Dockerfile .
+fi
+
 docker run -it --rm --gpus all \
 -v $path_code:/app/trainer \
 -v $path_logs:/logs -v $path_models:/models \
@@ -12,4 +16,4 @@ docker run -it --rm --gpus all \
 -e API_CHANNEL=$API_CHANNEL \
 -e API_F=$API_F \
 -e API_KEY=$API_KEY \
-trainer:latest
+maintrend-trainer:latest
