40e5335450f42758cbc1254a87ea251c9a008bc0
Zxited <mmgamesfull@gmail.com>
2020-05-04 18:03:44 +0200

    Trainer run change

--- trainer/build.sh
+++ trainer/build.sh
@@ -1,12 +1,12 @@
 path=/home/$(whoami)/container-data/
 path_logs=$path/logs/
 path_models=$(pwd)/models/
-path_code=$(pwd)/trainer/
 mkdir -p $path_logs
 mkdir -p $path_models
 
+docker build -t trainer:latest -f trainer/Dockerfile .
+
 docker run -it --rm --gpus all \
--v $path_code:/app/trainer \
 -v $path_logs:/logs -v $path_models:/models \
 -e API_BASE_URL=$API_BASE_URL \
 -e API_CHANNEL=$API_CHANNEL \
