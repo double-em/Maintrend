bd78a7767780235e9dfd82a96655e8da5ab37296
Zxited <mmgamesfull@gmail.com>
2020-05-17 17:09:19 +0200

    Basic API functionality implementation

--- serving-api/api.py
+++ serving-api/api.py
@@ -1,2 +1,27 @@
-def hello_world():
-    return "Hello!"
+import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI(title="Pedictor API", description="Serving version of the Predictor API.", version="1.2.1")
+class PredictionResult(BaseModel):
+    date: datetime.date = None
+    prediction_available: bool
+
+class PredictionRequest(BaseModel):
+    api_key: str
+    channel_id: int
+    prediction_date: datetime.date
+
+class APIStatus(BaseModel):
+    request_received: datetime.datetime = datetime.datetime.now()
+    internal_api_status: str = "Down"
+    messsage: str = "You've hit the API! Autch..."
+
+@app.get("/", response_model=APIStatus)
+async def root():
+    return APIStatus()
+
+@app.post("/predict", response_model=PredictionResult)
+async def predict(prediction_request : PredictionRequest):
+    if True:
+        return PredictionResult(prediction_available=False)
--- serving-api/main_v2.py
+++
@@ -1,5 +0,0 @@
-import connexion
-
-app = connexion.FlaskApp(__name__, specification_dir='openapi/')
-app.add_api('serving-api.yaml')
-app.run(port=5000)
